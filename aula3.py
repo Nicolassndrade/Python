a5cb2b2bf721b8869fe3cd2533e94c21db1f4cf4
Nicolassndrade <164218445+Nicolassndrade@users.noreply.github.com>
2024-08-29 11:57:20 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,16 @@
+# Variáveis em Python
+# Não há necessidade de declará-lás no início do programa 
+# Basta criar no meio do algoritmo com a atribuição
+
+teste = "Nicolas"
+print(teste)  # imprime a string "Nicolas"
+print(type(teste)) # imprime o tipo de variável, string
+teste = 10
+print(teste) # imprime o número 10
+print(type(teste)) # imprime o tipo de variável, que é int
+teste = 1.74
+print(teste) # imprime o número 1.74
+print(type(teste)) # imprime o tipo de variável, que é float
+teste = True
+print(teste) # imprime o número true
+print(type(teste)) # imprime o tipo de variável, que é bool  
