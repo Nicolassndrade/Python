35286134ee209a5fb7528469755c65b363f747f6
Nicolassndrade <164218445+Nicolassndrade@users.noreply.github.com>
2024-08-29 11:58:05 -0300

    Add files via upload

---
+++ nome-idade.py
@@ -0,0 +1,16 @@
+# atribuição de variáveis 
+nome = "Nicolas"
+idade = 16
+altura = 1.72
+
+
+# exibir mensagem na tela
+print("nome: ", nome, "idade: ", idade, "altura: ", altura)
+# formação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite a sua idade: "))
+altura = float(input("Digite a sua altura: "))
+
+print(f"nome: {nome} idade: {idade} altura: {altura}")
